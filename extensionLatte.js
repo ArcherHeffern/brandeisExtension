159e5bfa970b723ea3eda4ca492e32e7a8f28287
Archer Heffern <heffernarcher@gmail.com>
2022-06-26 17:03:31 -0400

    Am now able to print all data onto the screen after clearing all html css and js

--- extensionLatte.js
+++ extensionLatte.js
@@ -1,9 +1,9 @@
 const tables = document.getElementsByClassName('generaltable')
 const tablesandName = document.getElementsByClassName("collapsibleregion block-my-courses-collapsible-region")
-
-
+let tablesArray = []
 for (let i = 0; i < tables.length; i++) {
     console.log(tables[i])
+    tablesArray.push(tables[i].innerHTML)
 }
 
 for (let i = 0; i < tablesandName.length; i++) {
@@ -21,4 +21,10 @@ document.body.innerHTML = '';
 
 //set page data
 
-document.body.innerHTML = `<h1>Hello world</h1>`
+// document.body.append(tables[0])
+console.log(tablesArray[0]);
+
+
+for (let i = 0; i < tablesArray.length; i++) {
+    document.body.innerHTML += tablesArray[i] + "<br><br>"
+}
