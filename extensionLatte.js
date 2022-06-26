2ecb7b83c20a7c58d9c569b14ec3f1daa9e26a12
Archer Heffern <heffernarcher@gmail.com>
2022-06-26 16:40:39 -0400

    Removed all data from latte and input own data

--- extensionLatte.js
+++ extensionLatte.js
@@ -9,3 +9,16 @@ for (let i = 0; i < tables.length; i++) {
 for (let i = 0; i < tablesandName.length; i++) {
     console.log(tablesandName[i])
 }
+
+//delete all page data
+Array.prototype.forEach.call(document.querySelectorAll('link[rel=stylesheet]'), function (element) {
+    try {
+        element.parentNode.removeChild(element);
+    } catch (err) { }
+});
+
+document.body.innerHTML = '';
+
+//set page data
+
+document.body.innerHTML = `<h1>Hello world</h1>`
