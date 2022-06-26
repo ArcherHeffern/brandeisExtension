d924c6c65c4e284808174523fbd8172a674613f4
Archer Heffern <heffernarcher@gmail.com>
2022-06-26 17:08:12 -0400

    Tables are now fully formatted in table format, and extracted semester dates

--- extensionLatte.js
+++ extensionLatte.js
@@ -1,14 +1,14 @@
-const tables = document.getElementsByClassName('generaltable')
-const tablesandName = document.getElementsByClassName("collapsibleregion block-my-courses-collapsible-region")
-let tablesArray = []
-for (let i = 0; i < tables.length; i++) {
-    console.log(tables[i])
-    tablesArray.push(tables[i].innerHTML)
+// const tables = document.getElementsByClassName('generaltable')
+const tablesAndName = document.getElementsByClassName("collapsibleregion block-my-courses-collapsible-region")
+let tablesAndNameArray = []
+for (let i = 0; i < tablesAndName.length; i++) {
+    console.log(tablesAndNameArray[i])
+    tablesAndNameArray.push(tablesAndName[i].innerHTML)
 }
 
-for (let i = 0; i < tablesandName.length; i++) {
-    console.log(tablesandName[i])
-}
+// for (let i = 0; i < tablesandName.length; i++) {
+//     console.log(tablesandName[i])
+// }
 
 //delete all page data
 Array.prototype.forEach.call(document.querySelectorAll('link[rel=stylesheet]'), function (element) {
@@ -22,9 +22,9 @@ document.body.innerHTML = '';
 //set page data
 
 // document.body.append(tables[0])
-console.log(tablesArray[0]);
+console.log(tablesAndNameArray[0]);
 
 
-for (let i = 0; i < tablesArray.length; i++) {
-    document.body.innerHTML += tablesArray[i] + "<br><br>"
+for (let i = 0; i < tablesAndNameArray.length; i++) {
+    document.body.innerHTML += tablesAndNameArray[i] + "<br><br>"
 }
