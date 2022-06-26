81c920603f4e02d0518ef6d6146d7ea0d35a0cdb
Archer Heffern <heffernarcher@gmail.com>
2022-06-26 15:57:59 -0400

    Added 2 new files for latte, grabs course information to be used later

---
+++ extensionLatte.js
@@ -0,0 +1,11 @@
+const tables = document.getElementsByClassName('generaltable')
+const tablesandName = document.getElementsByClassName("collapsibleregion block-my-courses-collapsible-region")
+
+
+for (let i = 0; i < tables.length; i++) {
+    console.log(tables[i])
+}
+
+for (let i = 0; i < tablesandName.length; i++) {
+    console.log(tablesandName[i])
+}
