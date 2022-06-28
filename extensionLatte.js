9701511a626b0ae167c3897a2e1736d953af4cf3
Archer Heffern <heffernarcher@gmail.com>
2022-06-27 20:10:10 -0400

    Function to grab all semester names

--- extensionLatte.js
+++ extensionLatte.js
@@ -1,12 +1,24 @@
-// const tables = document.getElementsByClassName('generaltable')
+const timeline = document.getElementById("block-timeline-62b8d207039b862b8d206e627716")
 const tablesAndName = document.getElementsByClassName("collapsibleregion block-my-courses-collapsible-region")
+
+//selects all semester block htmls
 let tablesAndNameArray = []
 for (let i = 0; i < tablesAndName.length; i++) {
     console.log(tablesAndNameArray[i])
     tablesAndNameArray.push(tablesAndName[i].innerHTML)
 }
 //TODO: parse through tablesAndNameArray and create nested arrays of semesters and classes
+//!But why?
 //semesters => classes => classData
+
+const semesterClassClassDataList = []
+for (let i = 0; i < tablesAndName.length; i++) {
+    let semesterSectionHTML = tablesAndName[i]
+    //select semester name
+    let semester = semesterSectionHTML.querySelector(".collapsibleregioncaption a").textContent
+    //select courses
+    // semesterClassClassDataList = push([semester, [courseList]])
+}
 
 //delete all page data
 Array.prototype.forEach.call(document.querySelectorAll('link[rel=stylesheet]'), function (element) {
@@ -19,14 +31,6 @@ document.body.innerHTML = '';
 let courseContainer = document.createElement("div");
 courseContainer.className = "courseContainer"
 document.body.appendChild(courseContainer)
-
-
-
-//set page data
-
-// document.body.append(tables[0])
-// console.log(tablesAndNameArray[0]);
-
 
 for (let i = 0; i < tablesAndNameArray.length; i++) {
     document.querySelector(".courseContainer").innerHTML += tablesAndNameArray[i] + "<br><br>"
